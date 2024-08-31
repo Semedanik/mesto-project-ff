b2a31788196f05c4975cf6450601a1ca23b5e3b8
Danil Semenov <danil-semenov-2005@mail.ru>
2024-08-31 19:02:12 +1000

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,26 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const placesContainer = document.querySelector(".places__list");
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: Функция создания карточки
+function createCard(item, deleteCard) {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector(".card__image").src = item.link;
+  cardElement.querySelector(".card__title").textContent = item.name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+  const listItem = event.target.closest(".card");
+  listItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
-let str = 13;
+initialCards.forEach(function (item) {
+  const newCard = createCard(item, deleteCard);
+  placesContainer.append(newCard);
+});
